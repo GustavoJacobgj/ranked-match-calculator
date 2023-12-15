ab2e3cb8c4d3e32bedcd1b42ceb7030ca86a8918
Gustavo Jacob <gustavo.hcj@hotmail.com>
2023-12-15 19:48:40 -0300

    project finalized

---
+++ departure_calculator.js
@@ -0,0 +1,36 @@
+let result = calculateLevel("Tinhão", 69, 12);
+
+console.log(result);
+
+function calculateLevel(name, victories, defeats) {
+    let victoriesBalance = victories - defeats;
+
+    let level, message;
+
+    switch (true) {
+        case victories < 10:
+            level = "Iron";
+            break;
+        case victories >= 11 && victories <= 20:
+            level = "Bronze";
+            break;
+        case victories >= 21 && victories <= 50:
+            level = "Silver";
+            break;
+        case victories >= 51 && victories <= 80:
+            level = "Gold";
+            break;
+        case victories >= 81 && victories <= 90:
+            level = "Diamond";
+            break;
+        case victories >= 91 && victories <= 100:
+            level = "Legendary";
+            break;
+        default:
+            level = "Immortal";
+    }
+
+    message = `The Hero ${name} has a balance of ${victoriesBalance} and is at the ${level} level!`;
+
+    return message;
+}
